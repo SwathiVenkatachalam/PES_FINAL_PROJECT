8b2c37d29937c0957a2a557a9ceff0fe3b04bd94
SwathiVenkatachalam <112472328+SwathiVenkatachalam@users.noreply.github.com>
2024-11-02 15:20:54 -0400

    Updated led.h with comments

--- source/led.h
+++ source/led.h
@@ -7,23 +7,28 @@
  * @author      Swathi Venkatachalam
  * @tools       MCUXpresso IDE
  * @reference   https://github.com/alexander-g-dean/ESF/blob/master/NXP/Code/Chapter_8/I2C-Demo/inc/LEDs.h
+ *              FRDMKL25Z User manual
  *
  */
+// directives are used in header files to prevent multiple inclusions of same header file in single compilation unit, a technique called include guard.
+// prevents redefinition errors 
+#ifndef LED_H_ // Checks if LED_H_ has not been defined; #ifndef LED_H_ will evaluate to false, code within the guard will be skipped.
+// Defines LED_H_ to prevent future inclusions of this file
+#define LED_H_ 
 
-#ifndef LED_H_
-#define LED_H_
 
-// Freedom KL25Z LEDs
+// Freedom KL25Z LEDs from FRDM-KL25Z User's Manual Pg 12-13 (schematics and Connections Table)
 #define RED_LED_POS   (18)    // on port B
 #define GREEN_LED_POS (19)	  // on port B
 #define BLUE_LED_POS  (1)     // on port D
+// creates bit mask, that bit set to 1, all others 0
 #define MASK(x)       (1UL << (x))
 
 /*
  * @name   init_RGB_LEDs
  * @brief  Initializes RGB
  *
- * Initializes RGB
+ * Initializes RGB, GPIOs for LEDs
  *
  * @param  none
  * @return void
@@ -34,7 +39,7 @@ void init_RGB_LEDs();
  * @name   Control_RGB_LEDs
  * @brief  sets RGB
  *
- * sets RGB
+ * Turns RGB LEDs on and off
  *
  * @param  unsigned int red_on, unsigned int green_on, unsigned int blue_on
  * @return void
@@ -42,3 +47,4 @@ void init_RGB_LEDs();
 void Control_RGB_LEDs(unsigned int red_on, unsigned int green_on, unsigned int blue_on);
 
 #endif /* LED_H_ */
+// Ends the include guard
