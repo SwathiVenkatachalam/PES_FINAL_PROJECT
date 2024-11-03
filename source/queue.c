9f041bea658b6b85a113bfb5f442df326e1ab606
SwathiVenkatachalam <112472328+SwathiVenkatachalam@users.noreply.github.com>
2024-11-03 16:07:27 -0500

    Update queue.c with comments

--- source/queue.c
+++ source/queue.c
@@ -15,26 +15,8 @@
 #include "queue.h"
 
 
-#define INITIAL_VALUE (-1)
-#define ERROR         (-1)
-
-struct node
-{
-	void * data;
-	struct node *next;
-};
-typedef struct node node;
-struct cbfifo_s //LLFIFO definition
-{
-	struct node *first;
-	struct node *last;
-	struct node *store;
-	int q_length;
-	int q_capacity;
-};
-
-
-typedef struct cbfifo_s cbfifo;
+#define INITIAL_VALUE (-1) //Sets init val for front and rear indexes, indicating an empty state
+#define ERROR         (-1) //Used as return val for error conditions in queue ops
 
 /*
  * @name   cbfifo_create
@@ -47,19 +29,6 @@ typedef struct cbfifo_s cbfifo;
  */
 void cbfifo_create(Q_T * q)
 {
-	cbfifo *temp = (cbfifo*) malloc(sizeof(cbfifo)); //Create new LLFIFO
-	temp->q_capacity = Q_MAX_SIZE;
-	temp->q_length = 0;
-	temp->first = NULL;
-	temp->last = NULL;
-
-	node* temp_node = (node*) malloc(sizeof(node)); //Node creation
-	temp->first = temp->last = temp_node; //Front and rear pointers of FIFO assigned address of temp
-
-	temp->last->next = NULL; //Last node's next address pointed to null
-	temp->last = temp->store = temp->first; //Rear pointer set back to start node address
-	//temp;
-
 	q->front = INITIAL_VALUE;
 	q->rear  = INITIAL_VALUE;
 	q->length= 0;
@@ -103,23 +72,24 @@ int cbfifo_length(Q_T *q)
  * @param  void *buf, int nbyte, Q_T *q
  *         buf    Pointer to the data
  *         nbyte  Max number of bytes to enqueue
- *	       *q     Queue which needs to be enqueued (TxQ or RxQ)
+ *	     *q   Queue which needs to be enqueued (TxQ or RxQ)
  * @return int    number of bytes actually enqueued
  */
 int cbfifo_enqueue(void *buf, int nbyte, Q_T *q)
 {
 	int store = 0, enqbytes = 0, temp = 0, i = 0;
-	char *input=buf; //Character pointer created to point to buf
+	char *input = buf; //Character pointer created to point to buf
 
 	uint32_t masking_state;
 	masking_state = __get_PRIMASK(); 	// save current masking state
 	__disable_irq();          	// disable interrupts
+	
 	if(input == NULL) //If the buffer is NULL, error condition
 	{
 		__set_PRIMASK(masking_state); //Enable the interrupts again before returning
 		return ERROR;
 	}
-	else if((q->front==INITIAL_VALUE)&&(q->rear==INITIAL_VALUE)) //If no bytes is inserted, insert the first byte of data
+	else if((q->front==INITIAL_VALUE) && (q->rear==INITIAL_VALUE)) //If no bytes is inserted, insert the first byte of data
 	{
 		temp=INITIAL_VALUE; //variable used for enqueuing 1st byte in array
 		q->front=q->rear=0; //rear and front now at 0th index of array
@@ -127,32 +97,32 @@ int cbfifo_enqueue(void *buf, int nbyte, Q_T *q)
 		input++;
 		q->length++;
 	}
-	if (q->length==Q_MAX_SIZE||nbyte==0) //If the array is full or nbyte requested to front is zero
+	if (q->length == Q_MAX_SIZE || nbyte == 0) //If the array is full or nbyte requested to front is zero
 	{
 		__set_PRIMASK(masking_state);
 		return 0; //Indicate no bytes enqueued
 	}
-	else if(nbyte<=(Q_MAX_SIZE-q->length)) //If bytes to front is less or equal to available elements in array
+	else if(nbyte <= (Q_MAX_SIZE - q->length)) //If bytes to enqueue is less or equal to available space for new elements in array (max cap-used)
 	{
-		if(temp==INITIAL_VALUE) //To front remaining bytes after 1st byte was inserted in array when front and rear was -1
+		if(temp == INITIAL_VALUE) //To enqueue remaining bytes after 1st byte was inserted in array when write/front and read/rear was -1
 		{
-			for(i=0;i<nbyte-1;i++)
+			for(i = 0; i < nbyte - 1; i++)
 			{
 				q->front=(q->front+1)&(Q_MAX_SIZE-1); //To update front in such a way that on last index of array, after incrementing goes to start address
-				q->qdata[q->front]=*input; //1 byte inserted in array
-				input++;
-				q->length++;
+				q->qdata[q->front] = *input; //1 byte inserted in array
+				input++; //inc buffer ptr to next byte
+				q->length++; //inc length
 			}
-			temp=0; //Temp changed to zero as now the array has element
+			temp = 0; //Temp changed to zero as now the array has element
 			__set_PRIMASK(masking_state);
 			return nbyte; //Returns number of bytes enqueued
 		}
-		else //To front nbytes when front is not called first time and above if condition is met
+		else //To enqueue nbytes when enqueue is not called first time and above if condition is met
 		{
-			for(i=0;i<nbyte;i++)
+			for(i = 0; i < nbyte; i++)
 			{
 				q->front=(q->front+1)&(Q_MAX_SIZE-1);
-				q->qdata[q->front]=*input;
+				q->qdata[q->front] = *input;
 				input++;
 				q->length++;
 			}
@@ -160,30 +130,30 @@ int cbfifo_enqueue(void *buf, int nbyte, Q_T *q)
 			return nbyte;
 		}
 	}
-	else if(nbyte>(Q_MAX_SIZE-q->length)) //When bytes to front is more than the available space in array
+	else if(nbyte > (Q_MAX_SIZE - q->length)) //When bytes to enqueue is more than the available space in array
 	{
-		if(temp==-1) //To front remaining bytes after 1st byte was inserted in array when front and rear was -1
+		if(temp == -1) //To enqueue remaining bytes after 1st byte was inserted in array when write and read was -1
 		{
-			store=Q_MAX_SIZE-q->length-1;
-			for(i=0;i<store;i++)
+			store = Q_MAX_SIZE - q->length - 1; //available space
+			for(i = 0;i < store; i++)
 			{
-				q->front=(q->front+1)&(Q_MAX_SIZE-1);
-				q->qdata[q->front]=*input;
+				q->front = (q->front+1)&(Q_MAX_SIZE-1);
+				q->qdata[q->front] = *input;
 				input++;
 				q->length++;
 				enqbytes++;
 			}
-			temp=0;
+			temp = 0;
 			__set_PRIMASK(masking_state);
 			return enqbytes;
 		}
-		else //To front nbytes when front is not called first time and above if condition is met
+		else  //To enqueue nbytes when enqueue is not called first time and above if condition is met
 		{
-			store=Q_MAX_SIZE-q->length;
-			for(i=0;i<store;i++)
+			store = Q_MAX_SIZE - q->length;
+			for(i = 0; i < store; i++)
 			{
-				q->front=(q->front+1)&(Q_MAX_SIZE-1);
-				q->qdata[q->front]=*input;
+				q->front = (q->front+1)&(Q_MAX_SIZE-1);
+				q->qdata[q->front] = *input;
 				input++;
 				q->length++;
 				enqbytes++;
@@ -195,7 +165,7 @@ int cbfifo_enqueue(void *buf, int nbyte, Q_T *q)
 	else
 	{
 		__set_PRIMASK(masking_state);
-		return 0; //If no conditions above met, front operation is not performed
+		return 0; //If no conditions above met, enqueue operation is not performed, return 0 bytes enqueued
 	}
 }
 
@@ -219,29 +189,30 @@ int cbfifo_dequeue(void *buf, int nbyte, Q_T *q)
 	masking_state = __get_PRIMASK(); 	// save current masking state
 	__disable_irq(); 	// disable interrupts
 
-	if((q->front == INITIAL_VALUE && q->rear == INITIAL_VALUE) ||
-	                                            q->length == 0 ||
+	//Check if queue empty and input validation; buf where i/p val dequeued data would be copied is NULL; requested number of bytes to dequeue (nbyte) is zero
+	if(((q->front == INITIAL_VALUE) && (q->rear == INITIAL_VALUE)) ||
+	                                                q->length == 0 ||
 		                                          buf == NULL  ||
 		                                          nbyte == 0) //If the array is empty , return that 0 bytes were reard
 	{
 		__set_PRIMASK(masking_state);
-		return 0;
+		return 0; //0 bytes dequeued
 	}
 
-	else if (nbyte > q->length) //If byte to rear is more than length, rear all bytes available and return that
+	else if (nbyte > q->length) //If byte to dequeue is more than length, dequeue all bytes available and return that
 	{
-		int deqbyte = q->length; //Since array contains length elements, all are reard
+		int deqbyte = q->length; //Since array contains length elements, all are dequeued
 		for(int i = 0; i < deqbyte; i++)
 		{
 			*output = q->qdata[q->rear]; //dequeued data stored
 			q->rear = (q->rear+1) & (Q_MAX_SIZE-1); //rear updated to next array index
 			q->length--; //Length reduced
-			output++;
+			output++; //buffer inc
 		}
 		__set_PRIMASK(masking_state);
 		return deqbyte; //Return bytes dequeued
 	}
-	else if (nbyte <= q->length) //If byte to rear is less than or equal to length, rear those bytes
+	else if (nbyte <= q->length) //If byte to dequeue is less than or equal to length, dequeue those bytes
 	{
 		for(int i = 0; i < nbyte; i++)
 		{
@@ -255,7 +226,7 @@ int cbfifo_dequeue(void *buf, int nbyte, Q_T *q)
 	}
 	else
 	{
-		__set_PRIMASK(masking_state);
+	    __set_PRIMASK(masking_state);
 	    return 0; //If no conditions above met, rear operation is not performed
 	}
 }
